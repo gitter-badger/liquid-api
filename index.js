5fa1dabe93bffc8132d79532452d8f25b768b138
David Ernst <git@dsernst.com>
2016-09-21 14:04:41 -0700

    Create basic endpoint POST /voters to receive required fields

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express')
 var app = express()
 
+app.use(require('body-parser').json())
+
 app.get('/', (req, res) => res.send('Hello world'))
+app.post('/voters', require('./create-voter.js'))
 
 app.listen(3000, () => console.log('Server listening on port 3000'))
