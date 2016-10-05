69d46243e628a51baddf123ba3ad1d25d43d9782
David Ernst <git@dsernst.com>
2016-10-04 21:28:41 -0700

    Use Universal Link app.liquid.vote instead of liquidvote:// to login

--- send-login-email.js
+++ send-login-email.js
@@ -4,5 +4,5 @@ module.exports = (email, sessionId) => mailgun.messages().send({
   from: `Liquid Vote <info@${process.env.MAILGUN_DOMAIN}>`,
   to: email,
   subject: 'Sign in to Liquid Democracy',
-  html: `Click this magic link to sign in: liquidvote://?secret=${sessionId}`,
+  html: `Click this magic link to sign in: https://app.liquid.vote/?secret=${sessionId}`,
 })
