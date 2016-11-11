603bc8b5a098009a988cceb9fafb6536cad3f34a
David Ernst <git@dsernst.com>
2016-11-10 18:21:58 -0800

    Add geolocation to GET /location

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 Backend API to manage a liquid democracy
 
 See Github Issues for discussion -> https://github.com/crowdocracy/liquid-api/issues
+
+
+## Install
+
+```
+npm install
+```
+
+- Set credentials in `.env`
--- get-location.js
+++ get-location.js
@@ -5,6 +5,14 @@
 // Response:
 //
 
+const geoip2ws = require('geoip2ws')(process.env.MAXMIND_USERID, process.env.MAXMIND_LICENSE_KEY)
+
 module.exports = (req, res) => {
-  res.send(`Hi ${req.ips}🐬`)
+
+  const ip = req.ips[0] || process.env.EXAMPLE_IP
+
+  geoip2ws(ip, (err, geo) => {
+    console.log('geo:', geo)
+    res.send(`🐬 Hi \n\n ${JSON.stringify(geo)}`)
+  })
 }
