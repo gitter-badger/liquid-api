675da229d64f4e1d888028cd5672ab8043765b49
David Ernst <git@dsernst.com>
2016-11-24 10:53:51 -0600

    Add GET /recalldonald for vote totals

---
+++ get-recall-donald-votes.js
@@ -0,0 +1,47 @@
+// GET /votes/:voter_id — Get an individual's direct votes on a bill
+//
+// Response:
+//
+// {
+//   status: 201,
+//   body: {
+//   'BERNIE SANDERS': 17,
+//   'ED MCMULLEN': 11,
+//   'ELIZABETH WARREN': 25,
+//   'GARY JOHNSON': 13,
+//   'HILLARY CLINTON': 22,
+//   'JILL STEIN': 11,
+//   'JOE BIDEN': 23,
+//   'JOHN KASICH': 30,
+//   'MIKE PENCE': 5,
+//   'MITT ROMNEY': 29,
+//   'PAUL RYAN': 26,
+//   'TED CRUZ': 7,
+//   }
+// }
+//
+//
+
+const Promise = require('bluebird')
+const r = require('rethinkdb')
+
+module.exports = (req, res) => {
+  Promise.props({
+    votes: r.db('lv').table('recallDonald').reduce((left, right) => ({
+      'HILLARY CLINTON': left('HILLARY CLINTON').add(right('HILLARY CLINTON')),
+      'TED CRUZ': left('TED CRUZ').add(right('TED CRUZ')),
+      'BERNIE SANDERS': left('BERNIE SANDERS').add(right('BERNIE SANDERS')),
+      'MIKE PENCE': left('MIKE PENCE').add(right('MIKE PENCE')),
+      'JOHN KASICH': left('JOHN KASICH').add(right('JOHN KASICH')),
+      'MITT ROMNEY': left('MITT ROMNEY').add(right('MITT ROMNEY')),
+      'PAUL RYAN': left('PAUL RYAN').add(right('PAUL RYAN')),
+      'JOE BIDEN': left('JOE BIDEN').add(right('JOE BIDEN')),
+      'GARY JOHNSON': left('GARY JOHNSON').add(right('GARY JOHNSON')),
+      'ED MCMULLEN': left('ED MCMULLEN').add(right('ED MCMULLEN')),
+      'JILL STEIN': left('JILL STEIN').add(right('JILL STEIN')),
+      'ELIZABETH WARREN': left('ELIZABETH WARREN').add(right('ELIZABETH WARREN')),
+    })).run(req.app.locals.dbConn),
+    numVoters: r.db('lv').table('recallDonald').count().run(req.app.locals.dbConn),
+  })
+  .then(result => res.status(200).send(result))
+}
