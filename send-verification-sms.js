ac85ac95d9fe5727f72f88b2f344db1b3bea2e67
David Ernst <git@dsernst.com>
2017-01-12 10:28:18 -0500

    Add text for verification sms

---
+++ send-verification-sms.js
@@ -0,0 +1,16 @@
+const twilio = require('twilio')
+
+const client = new twilio.RestClient(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN)
+
+module.exports = (phone, sessionId) => client.messages.create({
+  body: `Are you ready for Liquid Democracy? Click here to verify your phone: https://app.liquid.vote/?secret=${sessionId}`,
+  // TODO: change link to https://app.liquid.vote/confirm/aec4b2a
+  to: `+1${phone}`,  // Text this number
+  from: '+14159692808',
+}, (err) => {
+  if (err) {
+    return console.error(err)
+  }
+
+  // console.log(message.sid)
+})
