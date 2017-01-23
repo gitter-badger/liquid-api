c9c0234560e28c835224533a5f3f96538deaafcf
David Ernst <git@dsernst.com>
2017-01-22 21:51:56 -0800

    Add GET /votes/bill/:bill_id to get direct vote counts

---
+++ get-bill-votes.js
@@ -0,0 +1,31 @@
+// GET /votes/bill/:bill_id
+//
+// Hit this endpoint to get direct vote counts on a bill
+//
+// Response:
+//
+//  {
+//    yea: 7,
+//    nay: 4,
+//    abstain: 1
+//  }
+//
+
+const r = require('rethinkdb')
+
+module.exports = (req, res) => {
+  r.table('votes')
+
+  .filter({ bill_id: req.params.bill_id })
+
+  // Count the positions at the db level
+  .group('position').count()
+  .run(req.app.locals.dbConn)
+
+  // Simplify db response syntax
+  .reduce((memo, position) => (Object.assign(memo, {
+    [position.group]: position.reduction,
+  })), {})
+
+  .then(results => res.status(200).send(results))
+}
