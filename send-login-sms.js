1d8cd89f861a0f6112a177f511cdda1fd4c2f911
David Ernst <git@dsernst.com>
2017-01-27 01:50:28 -0800

    Tweak SMS sign-in text

--- send-login-sms.js
+++ send-login-sms.js
@@ -3,7 +3,7 @@ const twilio = require('twilio')
 const client = new twilio.RestClient(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN)
 
 module.exports = (phone, sessionId) => client.messages.create({
-  body: `Hi, click this magic link to sign in: https://app.liquid.vote/?secret=${sessionId}`,
+  body: `Click this magic link to sign in to LIQUIDVOTE: https://app.liquid.vote/?secret=${sessionId}`,
   to: `+1${phone}`,  // Text this number
   from: `+1${process.env.TWILIO_NUMBER}`,
 }, (err) => {
