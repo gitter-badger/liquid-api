4080746fdc30cbacf501f01f7de391f1aa394d8b
David Ernst <git@dsernst.com>
2017-02-01 23:32:16 -0800

    Fix phone confirmation response to JSON can be properly parsed

--- confirm-phone.js
+++ confirm-phone.js
@@ -14,7 +14,10 @@
 //
 // {
 //   status: 202,
-//   message: "Confirmed: {voter_id}"
+//   body: {
+//     text: 'Confirmed.',
+//     registrationId: voter_id,
+//   }
 // }
 //
 // {
@@ -55,6 +58,6 @@ module.exports = (req, res) => {
     .update({ phone_confirmed_at: new Date() })
     .run(req.app.locals.dbConn)
     // Send success reply
-    .then(() => res.send(res.status(202).send(`Confirmed: ${voter.id}`)))
+    .then(() => res.send(res.status(202).send({ text: 'Confirmed.', registrationId: voter.id })))
   })
 }
