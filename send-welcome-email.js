0992996aaf9f35f0b62894e6539c34f6ad9e4e23
David Ernst <git@dsernst.com>
2017-02-06 01:04:31 -0800

    Simplify welcome email body

--- send-welcome-email.js
+++ send-welcome-email.js
@@ -7,15 +7,14 @@ module.exports = email => mailgun.messages().send({
   subject: 'Liquid Democracy signup confirmed',
   html: `Hi,
 
-  This confirms your registration to stay tuned about Liquid Democracy. Thank you.
+  This confirms your registration for Liquid Democracy. Thank you.
 
   We'll keep you in the loop about major announcements, such as when the first version of the Liquid Democracy app is available.
 
   For now, you're invited to come say hi in <a href="https://gitter.im/liquidvote/org">our chatroom</a> and share links about Liquid Democracy with your friends and family:
 
-  <em>Liquid Democracy promises to empower our most trustworthy leaders, give us true choice and accountability, and transform our politics and society. It does this all without requiring more effort for the voter than our current representative system.</em>
+  <em>Liquid Democracy promises to empower our most trustworthy leaders, give us true choice and accountability, and transform our politics and society.</em>
 
   Learn more at <a href="http://liquid.vote">http://liquid.vote</a>.
-
-  Thanks!`.replace(/(\n)/g, '<br />'),
+  `.replace(/(\n)/g, '<br />'),
 })
