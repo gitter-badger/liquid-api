652bbc78d52e67e1a60fbcbac94d7e9bf4932dfd
David Ernst <git@dsernst.com>
2017-02-08 16:42:48 -0800

    Include vote_uid and instructions to recreate hash in vote verification

--- publish-vote-verification.js
+++ publish-vote-verification.js
@@ -4,6 +4,9 @@
 //
 // Response:
 //
+//   VOTES FOR 2017-02-07-170007
+//
+//
 //   77a14de056bf7ee73629501fd7942c41e59dc857c404b4f6f426a3c2fdbfbab2 yea
 //   609a0a0f6a08067930e5671ff7ac768323b9d4694f4d201622b1db3d5c271df8 yea
 //   3856f0a7b52a3b03f3faffa8477115c5aac1bf22632294400c649cb09d9ca532 yea
@@ -11,12 +14,20 @@
 //   fcd651f744bef16442162c166df2e06d439a472820917d976cd5d3e8acbb5ced yea
 //   039d4af6f0cceebce7b9cd0f28243861cdea8ca86118e8e108a5d7a865759e0f nay
 //
+//
+//   ------------------------------------------------
+//   Hash generated from:
+//   sha3_256(voter_id + bill_uid + position + secret)
+//
 
 const hash = require('js-sha3').sha3_256
 const _ = require('lodash')
 const r = require('rethinkdb')
 
 module.exports = (req, res) => {
+
+  // Response is plain text, to format newlines
+  res.set('Content-Type', 'text/plain')
 
   // Get all votes on the bill
   r.table('votes').filter({ bill_uid: req.params.bill_uid }).run(req.app.locals.dbConn)
@@ -28,7 +39,16 @@ module.exports = (req, res) => {
   // Calculate hash
   .reduce((memo, vote) => (
     `${memo + hash(vote.voter_id + vote.bill_uid + vote.position + vote.secret)} ${vote.position}\n`
-  ), '')
+  ), `VOTES FOR ${req.params.bill_uid}\n\n\n`)
+
+  // Append info about hash
+  .then(verificationString => `${verificationString}
+
+------------------------------------------------
+Hash generated from:
+sha3_256(voter_id + bill_uid + position + secret)
+
+`)
 
   .then(results => res.status(200).send(results))
 }
